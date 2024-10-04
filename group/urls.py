eccaf263dab1beb47ecce3514039fbe816d9960f
9730991252 <sunilkale101@gmail.com>
2024-10-04 18:51:11 +0530

    ok

--- group/urls.py
+++ group/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:leader_id>/<int:village_id>/', views.group, name='group')
+    path('<int:leader_id>/', views.group, name='group')
 ]
