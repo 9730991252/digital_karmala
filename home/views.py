e88478f9a3368a8d97e893d79de52866b636ccca
9730991252 <sunilkale101@gmail.com>
2024-10-06 11:41:05 +0530

    ok

--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ from user.models import *
 # Create your views here.
 def index(request):
     #del request.session['user_mobile']
-    Chat_message.objects.all().delete()
     context={
         'leader':Leader.objects.filter(status=1)
     }
