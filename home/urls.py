4f359f3ca03977cf00785e17bbfd1acca594c8b8
9730991252 <sunilkale101@gmail.com>
2024-10-13 14:26:04 +0530

    ok

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('contact_us/', views.contact_us,name='contact_us'),    
     path('about_us/', views.about_us,name='about_us'),
     path('features/', views.features,name='features'),
+    path('privacy_policy/', views.privacy_policy,name='privacy_policy'),
+    path('terms_of_service/', views.terms_of_service,name='terms_of_service'),
+    path('disclaimer/', views.disclaimer,name='disclaimer'),
 ]
--- home/views.py
+++ home/views.py
@@ -16,3 +16,9 @@ def about_us(request):
     return render(request, 'home/about_us1.html')
 def features(request):
     return render(request, 'home/features.html')
+def privacy_policy(request):
+    return render(request, 'home/privacy_policy.html')
+def terms_of_service(request):
+    return render(request, 'home/terms_of_service.html')
+def disclaimer(request):
+    return render(request, 'home/disclaimer.html')
